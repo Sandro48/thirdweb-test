d8f37caac0dc372d4295ddd87d2143892dacc32a
Sandro Wegmann <sandrowegmann02@gmail.com>
2023-09-07 09:25:15 +0200

    gg

--- sendToken.js
+++ sendToken.js
@@ -1,6 +1,5 @@
-import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+const { ThirdwebSDK } = require("@thirdweb-dev/sdk");
 require('dotenv').config();
-import abi from './abi.json'
 
 const sdk = ThirdwebSDK.fromPrivateKey(
     process.env.SENDER_WALLET_PRIVATE_KEY, // Your wallet's private key (only required for write operations)
@@ -8,25 +7,52 @@ const sdk = ThirdwebSDK.fromPrivateKey(
     {
         secretKey: process.env.THIRDWEB_SECRET, // Use secret key if using on the server, get it from dashboard settings
     },
+
 );
 
-export default async function sendTokenTest() {
+async function sendTokenTest() {
 
-    const contract = await sdk.getContract("0xc2132D05D31c914a87C6611C10748AEb04B58e8F", abi);
+    const contract = await sdk.getContract("0xc2132D05D31c914a87C6611C10748AEb04B58e8F");
 
     // Address of the wallet you want to send the tokens to
-    const fromAddress = "...";
-    const toAddress = "...";
+    const fromAddress = process.env.SENDER_WALLET_PUBLIC_KEY;
+    const toAddress = process.env.RECEIVER_WALLET_PUBLIC_KEY;
     // The amount of tokens you want to send
-    const amount = 1.1;
+    const amount = 1000000;
 
 
-    const txResultD = await contract.erc20.balanceOf(fromAddress)
-    console.log(txResultD)
-    const txResult = await contract.erc20.transfer(toAddress, amount)
-    console.log(txResult)
+    // const txResultD = await contract.erc20.balanceOf(fromAddress)
+    // console.log(txResultD)
+    // const txResult = await contract.erc20.transfer(toAddress, amount)
+    // console.log(txResult)
+
+    const balanceData = await contract.call(
+        'balanceOf',
+        [
+            fromAddress
+        ],
+        {
+            ma
+        }
+    );
+    console.log(balanceData)
+
+
+    const data = await contract.call(
+        'transfer',
+        [
+            toAddress,
+            amount
+        ],
+
+
+    )
+
+    console.log(data)
 
 
 
 }
+
+sendTokenTest()
 
